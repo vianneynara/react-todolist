4ce5745b65b6552b9d5a86cf113c5723c4a9ac06
Nara <98963440+vianneynara@users.noreply.github.com>
2024-10-15 20:46:48 +0700

    change css
    Took 12 seconds

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
-import './tailwind.css'
-import './App.css'
+import './css/tailwind.css'
+import './css/App.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
